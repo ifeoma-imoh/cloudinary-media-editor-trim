5676dbfd436603aaf588ac4309d701079c77b697
ifeoma-imoh <imoh_ifeoma@yahoo.co.uk>
2022-07-13 13:15:01 +0100

    initial commit

---
+++ .env
@@ -0,0 +1,2 @@
+export const cloudName = process.env.NEXT_PUBLIC_CLOUD_NAME;
+export const uploadPreset = process.env.NEXT_PUBLIC_UPLOAD_PRESET;
--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { cloudName, uploadPreset } from "./cloudinary/cloudinaryConfig";
+import { useState } from "react";
 
 function App() {
+  const [dur, setDur] = useState(5)
+  function editVideo(publicId) {
+    
+    /* eslint-disable */
+    const myEditor = cloudinary.mediaEditor();
+    myEditor.update({
+      cloudName: cloudName,
+      publicIds: [
+        {
+          publicId: publicId,
+          resourceType: "video",
+        },
+      ],
+      video: {
+        steps: ["trim"],
+        trim: {
+          startOffset: 0,
+          endOffset: dur,
+          maxDuration: 10,
+          minDuration: 4
+        },
+      },
+    });
+    myEditor.show();
+    myEditor.on("export", function (data) {
+      console.log(data);
+      window.open(data.assets[0].downloadUrl);
+    });
+  }
+
+  const handleUpload = async (e) => {
+    const clUrl = `https://api.cloudinary.com/v1_1/${cloudName}/video/upload`;
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("upload_preset", uploadPreset);
+    const res = await fetch(clUrl, {
+      method: "POST",
+      body: formData,
+    });
+    const data = await res.json();
+    const publicId = data.public_id;
+    const duration = data.duration
+    editVideo(publicId);
+    setDur(duration)
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="file" accept="video/*" onChange={handleUpload} />
     </div>
   );
 }
